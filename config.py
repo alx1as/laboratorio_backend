a046bddf3c3670dce3ecbee4f984a11693958c3e
alx1as <alexiarubin23@gmail.com>
2025-11-26 11:11:53 -0300

    Update SQLALCHEMY_DATABASE_URI path in config.py
    SACANDO LA FUCKING RUTA RELATIVA

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = "supersecret"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = "sqlite:////home/site/wwwroot/data.db"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
