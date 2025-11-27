8f8265c9cddeb7dc7dbc47340a88bc8b0011426c
Alx1as <alexiarubin23@gmail.com>
2025-11-27 17:27:33 -0300

    limpiando base

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = "supersecret"
-    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = "sqlite:////home/site/wwwroot/data.db"
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = "sqlite:////home/site/wwwroot/nuevo_data.db"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
